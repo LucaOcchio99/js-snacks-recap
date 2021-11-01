b840ab2748470289f2f6e4741b7c198e6671203b
LucaOcchio99 <occhionorelli.luca99@gmail.com>
2021-11-01 17:20:04 +0100

    end

--- js-snacks-bonus-pomeriggio/main.js
+++ js-snacks-bonus-pomeriggio/main.js
@@ -5,26 +5,44 @@
 // le cifre che lo compongono stampandola nel DOM,
 //altrimenti ripeterà la richiesta di inserimento 
 //finché non avrà inserito un numero corretto
-const bottone = document.getElementById('btn');
+const bottone = document.querySelector('.btn');
 
-let numero_utente = '';
-bottone.addEventListner('click', function() {
-    numero_utente = prompt('Inserisci un numero di quattro cifre');
+//bottone.addEventListner('click', function(){
+//    let numero = prompt('inserisci un numero di 4 cifre');
+//    let somma = 0;
+//    let media = 0;
 
-    while ((numero_utente.length > 4) || (numero_utente.length < 4)) {
-        numero_utente = prompt('Insersici un numero di 4 cifre');
-    }
-    let number = parseInt(numero_utente);
-    let num1 = parseInt(number[0]);
-    let num2 = parseInt(number[1]);
-    let num3 = parseInt(number[2]);
-    let num4 = parseInt(number[3]);
-
-    let somma = num1 + num2 + num3 + num4;
-    let media = somma / 4;
-
-    document.body.innerHTML = `<h3>la somma delle cifre è ${somma}.</h3> 
-    <h3> la media della somma delle cifre è ${media}.</h3>`;
+//    while ((numero_utente.length > 4) || (numero_utente.length < 4)) {
+//        numero_utente = prompt('Insersici un numero di 4 cifre');
+//    }
     
-});
+//    for (let i = 0; i < numero.length; i++) {
+//        somma += parseInt(numero[i]);
+//    }
+//    media = somma / numero.length;
+    
+//    document.body.innerHTML = `<h3>la somma delle cifre è ${somma}.</h3> 
+//    <h3> la media della somma delle cifre è ${media}.</h3>`;
+    
+//});
 
+
+
+
+//Creare due array che hanno un numero diverso di elementi
+//aggiungere elementi casuali all'array con meno elementi, finchè entrambi gli array 1
+
+const primo_array = ['a', 'b', 'c', 'd', 'e'];
+console.log(primo_array);
+const secondo_array = ['e', 'f', 'g'];
+
+
+while (primo_array.length !== secondo_array.length) {
+    if (primo_array.length < secondo_array.length) {
+        secondo_array.push('z'); 
+    } else {
+        primo_array.push('z');
+    }
+}
+
+console.log(secondo_array);
